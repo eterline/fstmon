10f6b222e8cce280a9ab795a061993e62567bf0c
EterLine <andrewalyuk@gmail.com>
2025-12-06 21:50:02 +0800

    Taskfile changes Xray api first methods

--- cmd/fstmon/main.go
+++ cmd/fstmon/main.go
@@ -8,89 +8,23 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/eterline/fstmon/internal/app"
-	"github.com/eterline/fstmon/internal/config"
-	"github.com/eterline/fstmon/internal/infra/http/common/security"
+	"github.com/eterline/fstmon/internal/infra/xrayapi"
 	"github.com/eterline/fstmon/internal/log"
-	"github.com/eterline/fstmon/pkg/toolkit"
-)
-
-func init() {
-	toolkit.RegisterCommand("newtoken", func(args ...string) error {
-		ti, err := security.NewIssuedTokenAuthProvide(true)
-		if err != nil {
-			return err
-		}
-		token, err := ti.Issue()
-		if err != nil {
-			return err
-		}
-		fmt.Printf("token: %s\n", token)
-		return nil
-	})
-}
-
-// -ladflags variables
-var (
-	CommitHash = "dev"
-	Version    = "dev"
-)
-
-var (
-	Flags = app.InitFlags{
-		CommitHash: CommitHash,
-		Version:    Version,
-	}
-
-	cfg = config.Configuration{
-		Log: config.Log{
-			LogLevel:      "info",
-			JSONlog:       false,
-			AccessLogFile: "stdout",
-		},
-		Server: config.Server{
-			Listen:     ":3000",
-			CrtFileSSL: "",
-			KeyFileSSL: "",
-		},
-		Secure: config.Secure{
-			AllowedSubnets: []string{},
-			AllowedHosts:   []string{},
-			AuthToken:      false,
-		},
-		Monitor: config.Monitor{
-			Cpu:       10,
-			Memory:    10,
-			System:    20,
-			Thermal:   20,
-			NetworkIO: 10,
-			DiskIO:    10,
-		},
-	}
 )
 
 func main() {
-	root := toolkit.InitAppStart(
-		func() error {
-			err, ok := app.RunAdditional()
-			if err != nil {
-				return err
-			}
+	lg := log.NewLogger("debug", false)
+	// app.Execute(root, Flags, cfg)
 
-			if ok {
-				os.Exit(0)
-			}
+	lg.Info("xray api init")
 
-			err = config.ParseArgs(&cfg)
-			if err != nil {
-				return err
-			}
-			return nil
-		},
-	)
+	api, err := xrayapi.New("10.192.0.52:3000")
+	if err != nil {
+		lg.Error("xray api conn error", "error", err.Error())
+		os.Exit(0)
+	}
+	defer api.Close()
 
-	logger := log.NewLogger(cfg.LogLevel, cfg.JSONlog)
-	root.Context = log.WrapLoggerToContext(root.Context, logger)
+	fmt.Println(api.GetTraffic(false))
 
-	app.Execute(root, Flags, cfg)
 }
--- go.mod
+++ go.mod
@@ -4,23 +4,45 @@ go 1.25.3
 
 require (
 	github.com/alexflint/go-arg v1.6.0
-	github.com/ghodss/yaml v1.0.0
+	github.com/ghodss/yaml v1.0.1-0.20220118164431-d8423dcdf344
 	github.com/go-chi/chi/v5 v5.2.3
 	github.com/google/go-cmp v0.7.0
 	github.com/google/uuid v1.6.0
 	github.com/prometheus/procfs v0.19.2
 	github.com/shirou/gopsutil/v4 v4.25.10
+	github.com/xtls/xray-core v1.251202.0
 	golang.org/x/sys v0.38.0
+	google.golang.org/grpc v1.77.0
 )
 
 require (
 	github.com/alexflint/go-scalar v1.2.0 // indirect
+	github.com/andybalholm/brotli v1.0.6 // indirect
+	github.com/cloudflare/circl v1.6.1 // indirect
 	github.com/ebitengine/purego v0.9.1 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/juju/ratelimit v1.0.2 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.12 // indirect
 	github.com/lufia/plan9stats v0.0.0-20251013123823-9fd1530e3ec3 // indirect
+	github.com/miekg/dns v1.1.68 // indirect
+	github.com/pires/go-proxyproto v0.8.1 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
+	github.com/quic-go/quic-go v0.57.1 // indirect
+	github.com/refraction-networking/utls v1.8.1 // indirect
+	github.com/sagernet/sing v0.5.1 // indirect
 	github.com/tklauser/go-sysconf v0.3.16 // indirect
 	github.com/tklauser/numcpus v0.11.0 // indirect
+	github.com/xtls/reality v0.0.0-20251014195629-e4eec4520535 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	golang.org/x/crypto v0.44.0 // indirect
+	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	golang.org/x/tools v0.38.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	lukechampine.com/blake3 v1.4.1 // indirect
 )
